ddfcfe58311b460e99a382beac32b743d0e6293b
jamie <jamiewan@gammonconstruction.com>
2020-08-25 09:54:52 +0800

    Add project files.

---
+++ jamie-devops/Controllers/GreetingController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace jamie_devops.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreetingController : ControllerBase
+    {
+        public string Get(int id)
+        {
+            return "Hello World!";
+        }
+    }
+}
---
+++ jamie-devops/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace jamie_devops.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
