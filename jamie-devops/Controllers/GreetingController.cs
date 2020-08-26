508d8a80839a4243148fc75911056b73bdaa215c
jamie <jamiewan@gammonconstruction.com>
2020-08-26 10:33:37 +0800

    v5.0

--- jamie-devops/Controllers/GreetingController.cs
+++ jamie-devops/Controllers/GreetingController.cs
@@ -13,7 +13,7 @@ namespace jamie_devops.Controllers
     {
         public string Get(int id)
         {
-            return "Hello World! v4.0";
+            return "Hello World! v5.0";
         }
     }
 }
